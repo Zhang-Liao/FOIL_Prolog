980f94810346904aaba3ead0b544d1aaacb68abb
Zhang-Liao <zhangliao714@gmail.com>
2023-06-03 13:57:03 +0200

    predicates

--- test_coq.pl
+++ test_coq.pl
@@ -24,3 +24,28 @@ length1([X|Rest], N) :-
 	N is NRest + 1 .
 
 test_cover :- covers(rule(intro(v_1), [hyps_plus(v_4,v_2,v_6),hyps_plus(v_4,v_2,v_5),hyps_plus(v_1,v_2,v_3), vertical(v_6,v_5)]), pos(1)).
+
+above(X, Y) :- nonvar(X), nonvar(Y), dif(X, Y), prefix(X, Y).
+below(X, Y) :- nonvar(X), nonvar(Y), dif(X, Y), prefix(Y, X).
+
+vertical(X, Y) :- nonvar(X), nonvar(Y), dif(X, Y), prefix(X, Y).
+vertical(X, Y) :- nonvar(X), nonvar(Y), dif(X, Y), prefix(Y, X).
+
+horizon([X], [Y]) :- nonvar(X), nonvar(Y), dif(X, Y).
+horizon([H|X], [H|Y]) :- nonvar(X), nonvar(Y), dif(X, Y), horizon(X, Y).
+
+% intro(1).
+goal_nat(1, [0]).
+goal_plus(1, [0, 1]).
+hyps_plus(1, a, [a, 0]).
+hyps_plus(1, a, [a, 0, 1]).
+goal_nat(2, [0]).
+goal_plus(2, [0, 1]).
+hyps_plus(2, a, [a, 0]).
+hyps_plus(2, a, [a, 1]).
+
+% difference ?
+intro1(V1) :- hyps_plus(V1, V2, V3), hyps_plus(V1, V2, V4), vertical(V3, V4).
+intro2(V1) :- hyps_plus(V1, V2, V3), hyps_plus(V1, V2, V4), above(V3, V4).
+intro3(V1) :- hyps_plus(V1, V2, V3), hyps_plus(V1, V2, V4), below(V3, V4).
+
